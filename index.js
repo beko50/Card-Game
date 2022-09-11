7936d5fb4ea7ba582b39d6a12323c1d329d40754
Beko <essiamah20@gmail.com>
2022-09-11 12:55:22 +0000

    Made some minor changes to the Javascript file

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-let firstCard = 0;
-let secondCard = 21;
-let sum = firstCard + secondCard +9;
-let hasBlackJack = false;
-let isAlive = true
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+const cards = [firstCard, secondCard]
+let sum = firstCard + secondCard;
+let hasBlackJack = false; //boolean value
+let isAlive = false;   //boolean value before game starts
 let message = ""
 
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardEl = document.getElementById("card-el")
+//Players info using OBJECTS
+let player = {
+    name : "Bernard",
+    chips : 456
+}
+player.chips = Math.floor(Math.random()*1000) // randomizes player chips
+let playerEl =document.getElementById("player-el") 
+playerEl.textContent = player.name + " : $" + player.chips
 
+function getRandomCard(){
+    return Math.floor(Math.random()*13) +1; //randomize numbers b/n 1 and 13
+}
 
 function startGame(){
-    sumEl.textContent = "Sum: " + sum
+    isAlive = true;
+    sumEl.textContent = "Sum: " + sum  //display result on sum element id
+    cardEl.textContent = "Card: " 
+
+    for(let i=0;i<cards.length;i++){
+        cardEl.textContent += cards[i] + " ";
+    }
+
     if(sum <= 20){
     message = "Do you want to draw a new card?"
 }else if(sum === 21){
@@ -22,5 +41,16 @@ function startGame(){
     isAlive = false
 }
 messageEl.textContent = "Message: " + message
+
 }
 
+function newCard(){
+    if(isAlive==true && hasBlackJack == false){
+        let card = getRandomCard();
+        sum+=card
+        //pushing the cards to the cards array
+        cards.push(card)
+        startGame()    //calls the function startGame 
+    }
+    
+}
